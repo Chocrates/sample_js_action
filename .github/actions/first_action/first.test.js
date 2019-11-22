41998e55537be178d42cee9cd396ca35b754b0f5
Chris McIntosh <chocrates@github.com>
2019-11-22 15:15:29 -0600

    Refactoring tests a bit and adding the param to the workflow

--- .github/actions/first_action/first.test.js
+++ .github/actions/first_action/first.test.js
@@ -1,13 +1,19 @@
 const core = require('@actions/core')
-
 const first = require('./first')
+
+beforeEach(() => {
+    jest.resetModules()
+    process.env['INPUT_AMAZING-CREATURE'] = 'person'
+})
+
+afterEach(() => {
+    delete process.env['INPUT_AMAZING-CREATURE']
+})
 
 describe('Debug action debug messages', () => {
     it('outputs a debug message', async () => {
-        process.env['INPUT_AMAZING-CREATURE'] = 'person'
         const debugMock = jest.spyOn(core,'debug')
         await first.run()
         expect(debugMock).toHaveBeenCalledWith('Hello! You are an amazing person!')
-        delete process.env['INPUT_AMAZING-CREATURE']
     })
 })
