d9f7073c4b65de36582351071a593249fb8e0910
Chris McIntosh <chocrates@github.com>
2019-12-04 12:29:00 -0600

    Trying to add a comment

--- .github/actions/issue_action/issue.js
+++ .github/actions/issue_action/issue.js
@@ -3,7 +3,22 @@ const github = require('@actions/github')
 
 run = async () => {
     try {
-        console.log({payload: github.context.payload})
+        core.debug({payload: github.context.payload})
+
+        const token = process.env['GITHUB_TOKEN']
+        const octokit = new github.GitHub(token)
+
+        const issue = github.context.payload.issue
+
+        if(github.context.payload.action !== 'opened' || !issue){
+            core.debug('Not a valid event')
+            return
+        }
+
+        await octokit.issues.createComment({issue_number: issue.number, content: 'test'})
+
+        core.debug('did we say something?')
+
     } catch (error) {
         core.setFailed('Issue-action failure: ${error}')
     }
